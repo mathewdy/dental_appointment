5b78731438da947efd9a164c191f1c05d1b90259
Ryan <ryantecling@gmail.com>
2025-05-02 15:52:09 +0800

    Update dashboard.php

--- modules/admin/dashboard.php
+++ modules/admin/dashboard.php
@@ -1,21 +1,38 @@
 <?php
-include('../connection/connection.php');
 session_start();
 ob_start();
+$first_name = $_SESSION['first_name'];
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '../includes/styles.php' ?>
     <title>Document</title>
 </head>
 <body>
-    <h2>Dashboard admin</h2>
 
-    <a href="add-dentist.php">Add Dentist</a>
-    <a href="view-dentist.php">View Dentists</a>
-    <a href="logout.php">Logout</a>
+    <div class="wrapper">
+        <?php include '../includes/sidebar.php'; ?>
+
+      <div class="main-panel">
+        <?php include '../includes/topbar.php'; ?>
+        <div class="container">
+          <div class="page-inner">
+            <div class="page-header">
+              <h4 class="page-title">Home</h4>
+            </div>
+            <div class="page-category">
+                <h1>Welcome <?= $first_name; ?></h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php include "../includes/scripts.php"; ?>
 </body>
 </html>
