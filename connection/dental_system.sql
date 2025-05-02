0e56cce963e41cc3a6aedd1174771494c9c5e6ea
mathewdy <mathewdalisay@gmail.com>
2025-05-03 00:00:05 +0800

    Updated DB

--- connection/dental_system.sql
+++ connection/dental_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2025 at 07:56 AM
+-- Generation Time: May 02, 2025 at 05:59 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -83,20 +83,22 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `date_of_birth` date NOT NULL,
   `date_created` date NOT NULL,
-  `date_updated` date NOT NULL
+  `date_updated` date NOT NULL,
+  `token` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `user_id`, `role_id`, `first_name`, `middle_name`, `last_name`, `mobile_number`, `email`, `password`, `date_of_birth`, `date_created`, `date_updated`) VALUES
-(5, '202510175', 2, 'Dolan', 'Nevada', 'Dakota', 'Jemima', 'wuri@mailinator.com', '$2y$10$FE4A96gNBJVa8vBCdT97ueEg5U4zEBy8lfGWvzHS3HolRY0Vidpii', '2005-08-17', '2025-05-02', '2025-05-02'),
-(6, '20256264', 2, 'Hilary', 'Branden', 'Gretchen', 'Knox', 'xemymyqaju@mailinator.com', '$2y$10$hcLbSs6VZtpJtGoUtfkLnecTWIwYJ9bwJ6IDxy61FePTzCQHgr1tG', '2015-01-28', '2025-05-02', '2025-05-02'),
-(7, '20257257', 2, 'Mechelle', 'Cody', 'Sybill', 'Imelda', 'hogefah@mailinator.com', '$2y$10$LErVETC750eOZ9CrDX9FYeucbWMFh13.SWkgfaX/2P43v1n56AzA6', '2010-07-23', '2025-05-02', '2025-05-02'),
-(8, '20258557', 3, 'Shellie', 'Blaze', 'Lacota', 'Maisie', 'peqo@mailinator.com', '$2y$10$h3RfPh.hh50p1npPWPevKOlqtdZPuWzpHEel6CKJxulp929AJtN7S', '2016-07-08', '2025-05-02', '2025-05-02'),
-(9, '20257019', 3, 'Demetria', 'Amy', 'Vladimir', 'Garrett', 'kiqem@mailinator.com', '$2y$10$I087h3h/ks4tmZEvsb81ZOw.EjRQdKAGk4Xr/CqiwPR0sqXGMuAce', '2007-02-12', '2025-05-02', '2025-05-02'),
-(10, '20257825', 3, 'Nola', 'Elton', 'Hillary', 'Arsenio', 'risizog@mailinator.com', '$2y$10$wXbZoNsaTYtCrbc7boBW6uF23.akb1RmtqtNCWXF4XnipcFrpNQDm', '1991-03-06', '2025-05-02', '2025-05-02');
+INSERT INTO `users` (`id`, `user_id`, `role_id`, `first_name`, `middle_name`, `last_name`, `mobile_number`, `email`, `password`, `date_of_birth`, `date_created`, `date_updated`, `token`) VALUES
+(5, '202510175', 2, 'mathew', 'dalisay', 'Dakota', 'Jemima', 'mathewdalisay1@gmail.com', '$2y$10$2keq.iS/tKxKIGpD51Kef.m7TgCHIxlljH88LWqsyEXa5Fcvh6ALi', '2005-08-17', '2025-05-02', '2025-05-02', '9eb35bf094469a6c10087defe5651560'),
+(6, '20256264', 2, 'Hilary', 'Branden', 'Gretchen', 'Knox', 'xemymyqaju@mailinator.com', '$2y$10$hcLbSs6VZtpJtGoUtfkLnecTWIwYJ9bwJ6IDxy61FePTzCQHgr1tG', '2015-01-28', '2025-05-02', '2025-05-02', ''),
+(7, '20257257', 2, 'Mechelle', 'Cody', 'Sybill', 'Imelda', 'hogefah@mailinator.com', '$2y$10$LErVETC750eOZ9CrDX9FYeucbWMFh13.SWkgfaX/2P43v1n56AzA6', '2010-07-23', '2025-05-02', '2025-05-02', ''),
+(8, '20258557', 3, 'Shellie', 'Blaze', 'Lacota', 'Maisie', 'peqo@mailinator.com', '$2y$10$h3RfPh.hh50p1npPWPevKOlqtdZPuWzpHEel6CKJxulp929AJtN7S', '2016-07-08', '2025-05-02', '2025-05-02', ''),
+(9, '20257019', 3, 'Demetria', 'Amy', 'Vladimir', 'Garrett', 'kiqem@mailinator.com', '$2y$10$I087h3h/ks4tmZEvsb81ZOw.EjRQdKAGk4Xr/CqiwPR0sqXGMuAce', '2007-02-12', '2025-05-02', '2025-05-02', ''),
+(10, '20257825', 3, 'Nola', 'Elton', 'Hillary', 'Arsenio', 'risizog@mailinator.com', '$2y$10$wXbZoNsaTYtCrbc7boBW6uF23.akb1RmtqtNCWXF4XnipcFrpNQDm', '1991-03-06', '2025-05-02', '2025-05-02', ''),
+(11, '20254583', 1, 'Iliana', 'Dennis', 'Beatrice', 'Aquila', 'mathewdalisay@gmail.com', '$2y$10$H/LEMI9kUujYIlIMvml/..vO45NpCg0pVFOg/xcmC6X/y27WLWTJK', '2010-11-28', '2025-05-02', '2025-05-02', '997ed20d681cef28b00bbc07a88c0b06');
 
 --
 -- Indexes for dumped tables
@@ -140,7 +142,7 @@ ALTER TABLE `schedule`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
