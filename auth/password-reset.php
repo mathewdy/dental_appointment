0e07c29d9b9a35b8f45d1673e3a6302b09c8016a
Ryan <ryantecling@gmail.com>
2025-05-04 23:49:22 +0800

    updated designs
    - other designs will be added asap

--- auth/password-reset.php
+++ auth/password-reset.php
@@ -22,6 +22,41 @@ include("../includes/header.php")
         <input type="hidden" name="token" value="<?php if(isset($_GET['token'])){echo $_GET['token'];} ?>">
         <input type="submit" name="update" Value="Update Password">
     </form>
+    <div class="container" style="height: 55em;">
+        <div class="row d-flex justify-content-center align-items-center p-5" style="height: 100%;">
+            <div class="col-12">
+                <div class="card w-100 border-none rounded-0" style="height: 45em;">
+                    <div class="row" style="height: 100%;">
+                        <div class="col-lg-6 p-5 d-flex flex-column justify-content-center ">
+                            <span class="text-center">
+                                <img src="../assets/img/logo.png" height="100" alt="">
+                            </span>
+                            <span class="mb-4 text-center">
+                                <h1>Sign In</h1>
+                            </span>
+                            <form action="" method="POST">
+                                <div class="row px-4">
+                                    <div class="col-lg-12 mb-5">
+                                        <label for="">Email</label>
+                                        <input type="email" class="form-control" name="email" placeholder="Email">
+                                    </div>
+                                    <div class="col-lg-12 mb-5">
+                                        <label for="">Password </label>
+                                        <input type="password" class="form-control" name="password" placeholder="Password">
+                                    </div>
+                                    <div class="col-lg-12 text-center">
+                                        <input type="submit" class="btn btn-black op-8 w-100 mb-2" name="login" value="Login">
+                                        <a href="forgot-password.php" class="text-dark">Forgot Password?</a>
+                                    </div>
+                                </div>                                                      
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include "../includes/scripts.php"; ?>
 </body>
 </html>
 
