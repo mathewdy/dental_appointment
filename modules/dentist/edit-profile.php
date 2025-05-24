a0ceafa0bd33941f18f6758d608ead4ec23551e9
mathewdy <88813049+mathewdy@users.noreply.github.com>
2025-05-24 10:32:54 +0800

    Update edit-profile.php

--- modules/dentist/edit-profile.php
+++ modules/dentist/edit-profile.php
@@ -6,9 +6,9 @@ ob_start();
 $first_name = $_SESSION['first_name'];
 $email = $_SESSION['email'];
 ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include('../../includes/security.php');
 
 ?>
 
