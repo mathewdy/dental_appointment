5db990ce783e9879f66d0d7f9a3bb75e64f67ed8
mathewdy <88813049+mathewdy@users.noreply.github.com>
2025-05-24 09:53:53 +0800

    finished dashboard
    2. dashboard
    
    Total Appointments Scheduled (OK)
    Total Appointments Completed (OK)
    Walk-in Appointments (OK)
    Cancellations (OK)

--- modules/admin/dashboard.php
+++ modules/admin/dashboard.php
@@ -31,6 +31,40 @@ $first_name = $_SESSION['first_name'];
             <div class="page-category">
                 <h1>Welcome <?= $first_name; ?></h1>
             </div>
+
+          <?php
+
+          $query_date_today = "SELECT COUNT(*) AS total_appointments,
+          SUM(CASE WHEN confirmed = 1 THEN 1 ELSE 0 END) AS total_completed,
+          SUM(CASE WHEN walk_in = 1 THEN 1 ELSE 0 END) AS total_walkins,
+          SUM(CASE WHEN confirmed = 2 THEN 1 ELSE 0 END) AS total_cancellations
+          FROM appointments
+          WHERE STR_TO_DATE(appointment_date, '%m/%d/%Y') = CURDATE();";
+          $run_date_today = mysqli_query($conn,$query_date_today);
+
+          if(mysqli_num_rows($run_date_today) > 0){
+            foreach($run_date_today as $row_date_today){
+              ?>
+
+              <h1>Total Appointments Scheduled: <?php echo $row_date_today['total_appointments']?></h1>
+              <h1>Total Appointments Completed: <?php echo $row_date_today['total_completed']?></h1>
+              <h1>Walk-in Appointments: <?php echo $row_date_today['total_walkins']?></h1>
+              <h1>Cancellations: <?php echo $row_date_today['total_cancellations']?></h1>
+
+              <?php
+            }
+          }
+
+
+          ?>
+
+          
+
+            <form action="" method="POST">
+              <input type="date" name="start_date">
+              <input type="date" name="end_date">
+              <input type="submit" name="filter" value="Filter">
+            </form>
           </div>
         </div>
       </div>
@@ -38,3 +72,34 @@ $first_name = $_SESSION['first_name'];
     <?php include "../../includes/scripts.php"; ?>
 </body>
 </html>
+
+<?php
+if (isset($_POST['start_date']) && isset($_POST['end_date'])) {
+  $start_date = $_POST['start_date'];
+  $end_date = $_POST['end_date'];
+
+  $filter_dates = "SELECT COUNT(*) AS total_appointments,
+                    SUM(CASE WHEN confirmed = 1 THEN 1 ELSE 0 END) AS total_completed,
+                    SUM(CASE WHEN walk_in = 1 THEN 1 ELSE 0 END) AS total_walkins,
+                    SUM(CASE WHEN confirmed = 2 THEN 1 ELSE 0 END) AS total_cancellations
+                FROM appointments
+                WHERE STR_TO_DATE(appointment_date, '%m/%d/%Y') 
+                      BETWEEN STR_TO_DATE('$start_date', '%Y-%m-%d') 
+                      AND STR_TO_DATE('$end_date', '%Y-%m-%d')";
+  $run_filter_dates = mysqli_query($conn, $filter_dates);
+
+  if ($run_filter_dates) {
+    $row_dates = mysqli_fetch_assoc($run_filter_dates);
+    ?>
+      <h2>Total Appointments: <?php echo $row_dates['total_appointments']; ?></h2>
+      <h2>Total Completed: <?php echo $row_dates['total_completed']; ?></h2>
+      <h2>Total Walk-ins: <?php echo $row_dates['total_walkins']; ?></h2>
+      <h2>Total Cancellations: <?php echo $row_dates['total_cancellations']; ?></h2>
+    <?php
+  } else {
+    echo "<h3>Query failed: " . mysqli_error($conn) . "</h3>";
+  }
+}
+
+
+?>
