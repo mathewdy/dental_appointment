050e5229acfc6ee1a917145a9ad7d8a8b625f1de
Ryan121716 <ryantecling@gmail.com>
2025-08-13 12:46:18 +0800

    function for notification creation

---
+++ modules/queries/notification.php
@@ -0,0 +1,11 @@
+<?php 
+
+function createNotification($conn, $user_id, $message, $type, $createdAt, $createdBy) {
+  $sql = "INSERT INTO `notification` (user_id, `message`, hasRead, `type`, createdAt, createdBy)
+    VALUES (?, ?, 0, ?, ?, ?)";
+  
+  $stmt = mysqli_prepare($conn, $sql);
+  mysqli_stmt_bind_param($stmt, "issds", $user_id, $message, $type, $createdAt, $createdBy); 
+  
+  return mysqli_stmt_execute($stmt);
+}
