05fd26cfd843e6385ef7f007321c3feff7d2de95
Raie0 <ryantecling@gmail.com>
2025-10-25 18:54:12 +0800

    up

--- modules/patients/requests.php
+++ modules/patients/requests.php
@@ -53,6 +53,7 @@ $user_id_patient = $_SESSION['user_id'];
                             <th>Appointment Date & Time</th>
                             <th>Dentist</th>
                             <th>Concern</th>
+                            <th>Status</th>
                             <th>Action</th>
                         </tr>
                     </thead>
@@ -67,26 +68,35 @@ $user_id_patient = $_SESSION['user_id'];
                                         <td>Dr. <?php echo $row_appointment['doctor_first_name'] . " " . $row_appointment['doctor_last_name']?></td>
                                     <td><?php echo $row_appointment['concern']?></td>
                                     <td>
-                                        <?php
-                                            $handler = match($row_appointment['confirmed']) {
-                                                1 => '<span class="badge bg-success">Confirmed</span>',
-                                                2 => '<span class="badge bg-danger">Cancelled</span>',
-                                                default => '
-                                                    <form action="declined.php" method="POST" onsubmit="return confirmCancel()">
-                                                        <input type="hidden" name="appointment_date" value="' . htmlspecialchars($row_appointment['appointment_date']) . '">
-                                                        <input type="submit" class="btn btn-danger btn-sm" name="decline" value="Cancel">
-                                                    </form>
-                                                    <script>
-                                                        function confirmCancel() {
-                                                            return confirm("Are you sure you want to cancel?");
-                                                        }
-                                                    </script>
-                                                ',
-                                            };
-                                            echo $handler;
-                                            ?>
-                                        </td>
-                                    </tr>
+                                      <?php
+                                        $handler = match($row_appointment['confirmed']){
+                                            1 => '<span class="badge text-dark text-body-secondary fw-bold" style="background: #94f7c9; border: #94f7c9;"><i class="fas fa-check-circle"></i> Confirmed</span>',
+                                            2 => '<span class="badge text-dark text-body-secondary fw-bold" style="background: #f79494; border: #f79494;"><i class="fas fa-times-circle"></i> Cancelled</span>',
+                                            3 => '<span class="badge text-dark text-body-secondary fw-bold" style="background: #fab273; border: #fab273;"><i class="fas fa-times"></i> No Show</span>',
+                                            default => '<span class="badge text-dark text-body-secondary fw-bold" style="background: #fae373; border: #fae373;"><i class="fas fa-clock"></i> Pending</span>'
+                                        };
+                                        echo $handler;
+                                      ?>
+                                    </td>
+                                    <td>
+                                      <?php
+                                        if($row_appointment['confirmed'] == 0) {
+                                          ?>
+                                          <div class="text-center text-danger">
+                                            <a href="request-action.php?id=<?= $row_appointment['appointment_id']?>&status=2" class="status"><i class="fas fa-trash"></i></a>
+                                          </div>
+                                          <?php
+                                        } else {
+                                          ?>
+                                          <div class="text-center text-danger" style="opacity: 50%;">
+                                            <a href="#" class="status" style="pointer-events:none; color"><i class="fas fa-trash"></i></a>
+                                          </div>
+                                          <?php
+                                        }
+                                      ?>
+                                      
+                                    </td>
+                                  </tr>
                                     <?php
                                 }
                             }
@@ -102,3 +112,11 @@ $user_id_patient = $_SESSION['user_id'];
 <?php 
 include_once($_SERVER['DOCUMENT_ROOT'] . '/dental_appointment/includes/scripts.php'); 
 ?>
+<script>
+$(document).ready(function() {
+  $('.status').on('click', function(e) {
+    e.preventDefault();
+    confirmBeforeRedirect("Do you want to update this appointment?", $(this).attr('href'))
+  });
+});
+</script>
