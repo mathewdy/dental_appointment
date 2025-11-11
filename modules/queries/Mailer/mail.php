e2fd7191340a212068206c18a6813fc1e9d3c476
mathewdy <88813049+mathewdy@users.noreply.github.com>
2025-11-11 21:50:37 +0800

    updated mailer

--- modules/queries/Mailer/mail.php
+++ modules/queries/Mailer/mail.php
@@ -14,12 +14,12 @@ function sendEmail($message, $title, $creds) {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';       
     $mail->SMTPAuth = true;
-    $mail->Username = 'matthewdalisay2001@gmail.com';
-    $mail->Password = 'mhqsbraxqhjzlelg';
+    $mail->Username = 'i05764152@gmail.com';
+    $mail->Password = 'hemgbpmzfozvjsiz';
     $mail->SMTPSecure = 'tls';            
     $mail->Port = 587;                      
 
-    $mail->setFrom('matthewdalisay2001@gmail.com', 'Dental Clinic');
+    $mail->setFrom('i05764152@gmail.com', 'Dental Clinic');
     $mail->addAddress($creds);    
 
     $mail->isHTML(true);                               
